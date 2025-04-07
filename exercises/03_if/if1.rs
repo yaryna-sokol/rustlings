d99368970bd33f9a12643394b6b6c0e1941073c7
yaryna-sokol <yaryna.sokol@mailbox.tu-dresden.de>
2025-04-07 18:23:18 +0200

    Update if1.rs

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -1,4 +1,9 @@
 fn bigger(a: i32, b: i32) -> i32 {
+    if a<b {
+        b
+    } else if a>b {
+        a
+    }
     // TODO: Complete this function to return the bigger number!
     // If both numbers are equal, any of them can be returned.
     // Do not use:
